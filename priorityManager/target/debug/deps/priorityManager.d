9d695033319d7f562d0957baaec1501ac014c169
Mood <mahmoodgladney@gmail.com>
2020-11-24 12:58:43 -0700

    added cargo and a gitignore that isn't helping

---
+++ priorityManager/target/debug/deps/priorityManager.d
@@ -0,0 +1,5 @@
+C:\Users\Mood\Documents\GitHub\CSC372-Final-Project-Rust\priorityManager\target\debug\deps\priorityManager.exe: src\main.rs
+
+C:\Users\Mood\Documents\GitHub\CSC372-Final-Project-Rust\priorityManager\target\debug\deps\priorityManager.d: src\main.rs
+
+src\main.rs:
---
+++ priorityManager/target/debug/priorityManager.d
@@ -0,0 +1,1 @@
+C:\Users\Mood\Documents\GitHub\CSC372-Final-Project-Rust\priorityManager\target\debug\priorityManager.exe: C:\Users\Mood\Documents\GitHub\CSC372-Final-Project-Rust\priorityManager\src\main.rs
